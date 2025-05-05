532a0ca884c3e3f43e1199908fc61bd039a344f7
marievalerie <79691910+marievalerie@users.noreply.github.com>
2025-05-05 11:56:47 +0200

    Add files via upload

---
+++ Timerseries_pooled_RNAseq/snakemake_mapping.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+#$ -S /bin/bash
+#$ -N snakejob
+#$ -cwd
+#$ -j n
+#$ -m e
+#$ -M m.brasseur@leibniz-zfmk.de
+
+module load anaconda3/2020.02
+conda activate snakemake
+module load samtools/1.10
+module load opt-python
+
+a=1; b=$NSLOTS
+THREADS=$((b-a))
+
+snakemake --cores ${THREADS} --snakefile Snakefile_hisat_mapping
